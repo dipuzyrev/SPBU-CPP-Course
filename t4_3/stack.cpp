73feff0a871318e2f5e14add98e8bdcea5c43199
Dmitry <dmitry@puzyrev.me>
2016-10-31 22:10:42 +0300

    t4_3 | first release

---
+++ t4_3/stack.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+struct StackElement
+{
+    StackElement *next;
+    char value;
+};
+
+struct Stack
+{
+    StackElement *top;
+    int size;
+};
+
+
+Stack *createStack()
+{
+    Stack *newStack = new Stack;
+    newStack->top = nullptr;
+    newStack->size = 0;
+    return newStack;
+}
+
+void push(Stack *s, char value)
+{
+    StackElement *elem = new StackElement;
+    elem->next = s->top;
+    elem->value = value;
+    s->top = elem;
+    s->size++;
+}
+
+bool isEmpty(Stack const *s)
+{
+    return s->size == 0;
+}
+
+char pop(Stack *s)
+{
+    if (isEmpty(s))
+        return 'e';
+
+    char value = s->top->value;
+
+    StackElement *toDelete = s->top;
+    s->top = s->top->next;
+    s->size--;
+    delete toDelete;
+
+    return value;
+}
+
+char top(Stack *s)
+{
+    if (isEmpty(s))
+        return 'e';
+    else
+        return s->top->value;
+}
