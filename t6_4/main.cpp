a5f1643a2ac237cf0e334629e8d394370d88f295
Dmitry <dmitry@puzyrev.me>
2016-11-02 21:27:06 +0300

    t6_4 | first release

---
+++ t6_4/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+bool isEmpty(char *str, int maxSize)
+{
+    for (int i = 0; i < maxSize; i++)
+    {
+        char s = str[i];
+        if (s != '\0' && s != '\t' && s != ' ')
+            return false;
+    }
+
+    return true;
+}
+
+void cleanBuffer(char *buffer, int maxSize)
+{
+    for (int i = 0; i < maxSize; i++)
+        buffer[i] = '\0';
+}
+
+int main()
+{
+    ifstream fin("input.txt");
+    if (!fin.is_open())
+    {
+        cout << "File not exist!" << endl;
+        return 0;
+    }
+
+    int const maxSize = 4096;
+    char buffer[maxSize] = {'\0'};
+    int countOfEmpty = 0;
+
+    while(!fin.eof())
+    {
+        fin.getline(buffer, maxSize);
+
+        if (isEmpty(buffer, maxSize))
+            countOfEmpty++;
+
+        cleanBuffer(buffer, maxSize);
+    }
+
+    cout << "Count of empty lines: " << countOfEmpty << endl;
+
+    fin.close();
+    return 0;
+}
