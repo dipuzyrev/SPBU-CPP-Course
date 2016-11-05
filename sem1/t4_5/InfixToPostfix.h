9751c2387a803d4335ea347aa465a094d188617e
Dmitry <dmitry@puzyrev.me>
2016-11-05 14:14:01 +0300

    t4_5 | fix bugs

---
+++ sem1/t4_5/InfixToPostfix.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void inputInfixExpression(char *infixExpression, int maxLength);
+void convertToPostfix(char *infixExpression, char *postfixExpression, int maxLength);
+void outputPostfix(char *postfixExpression);
--- sem1/t4_5/main.cpp
+++ sem1/t4_5/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "Stack.h"
+#include "InfixToPostfix.h"
 
 using namespace std;
 
-int getStrLength(char str[], int maxLength)
+int getCharLength(char str[], int maxLength)
 {
     int length = 0;
     for (int i = 0; i < maxLength; i++)
@@ -12,81 +13,6 @@ int getStrLength(char str[], int maxLength)
         else
             return length;
     return length;
-}
-
-void concatStr(char str1[], char str2[], int maxLength)
-{
-    int length1 = getStrLength(str1, maxLength);
-    int length2 = getStrLength(str2, maxLength);
-    int i = length1;
-    int j = 0;
-
-    while (i < maxLength && j < length2)
-    {
-        str1[i] = str2[j];
-        i++;
-        j++;
-    }
-}
-
-void concatChar(char str1[], char charToConcat, int maxStrLength)
-{
-    int strLength = getStrLength(str1, maxStrLength);
-    if (strLength != maxStrLength)
-        str1[strLength] = charToConcat;
-}
-
-bool mustPop(char token, char topOperator)
-{
-    if (token == ')')
-        return true;
-
-    if (token == '-' || token == '+')
-        if (topOperator == '+' || topOperator == '-' || topOperator == '*' || topOperator == '/')
-            return true;
-
-    if (token == '*' || token == '/')
-        if (topOperator == '*' || topOperator == '/')
-            return true;
-
-    return false;
-}
-
-void handleToken(char token, Stack *operators, char postfixExpression[], int maxLength)
-{
-
-    if (isdigit(token))
-    {
-
-        concatChar(postfixExpression, token, maxLength);
-        return;
-    }
-
-    if (isEmpty(operators) || !mustPop(token, top(operators)))
-    {
-        push(operators, token);
-        return;
-    }
-
-    char currentToken = token;
-    while(!isEmpty(operators) && mustPop(currentToken, top(operators)) && top(operators) != '(')
-    {
-        concatChar(postfixExpression, top(operators), maxLength);
-        currentToken = pop(operators);
-    }
-    if (top(operators) == '(')
-        pop(operators);
-    if (token != ')')
-        push(operators, token);
-}
-
-void addRestOperators(Stack *operators, char postfixExpression[], int maxLength)
-{
-    while (!isEmpty(operators))
-    {
-        char newOperator = pop(operators);
-        concatChar(postfixExpression, newOperator, maxLength);
-    }
 }
 
 int convertToNumber(char number)
@@ -119,34 +45,16 @@ int performOperation(char operation, int number1, int number2)
 
 int main()
 {
-    //input expression
-    cout << "Input your expression in infix notation: ";
     int const maxLength = 1024;
     char infixExpression[maxLength] = {' '};
-    cin.getline(infixExpression, maxLength);
+    inputInfixExpression(infixExpression, maxLength);
 
-    //create empty result str
     char postfixExpression[maxLength] = {' '};
+    convertToPostfix(infixExpression, postfixExpression, maxLength);
 
-    //cyrcle: handle tokens, concat to result str (if need)
-    Stack *operators = createStack();
-    int infixLength = getStrLength(infixExpression, maxLength);
-    for (int i = 0; i < infixLength; i++)
-    {
-        char currentTocken = infixExpression[i];
-        handleToken(currentTocken, operators, postfixExpression, maxLength);
-    }
-
-    //get all rest of operators in stack and concat to result str
-    addRestOperators(operators, postfixExpression, maxLength);
-
-
-
-
-    //postfix expression
     Stack *operands = createStack();
+    int length = getCharLength(postfixExpression, maxLength);
 
-    int length = getStrLength(postfixExpression, maxLength);
     for (int i = 0; i < length; i++)
     {
         char token = postfixExpression[i];
