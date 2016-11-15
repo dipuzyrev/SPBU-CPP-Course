1687e121a1eaa10e56a71767dea5e4cfbd5a3c52
Dmitry <dmitry@puzyrev.me>
2016-11-15 20:26:20 +0300

    6.3 | fix bugs

--- sem1/6/6.3/main.cpp
+++ sem1/6/6.3/main.cpp
@@ -22,23 +22,41 @@ void printSpaces(int count)
 }
 //int getMax(int n1, int n2);
 
+void inputCoefficients(int coefficients[], int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		cout << "Enter " << i << "th coefficient: ";
+		cin >> coefficients[length - 1 - i];
+	}
+
+	cout << endl;
+}
+
 int main()
 {
-    int coefficients[] = {-112, -5, 561, 9};
-    int length = 4;
+	cout << "Enter coefficients count: ";
+	int length = 0;
+	cin >> length;
+    int coefficients[length] = {0};
+    inputCoefficients(coefficients, length);
 
     //print 1 line
-
+    cout << " ";
     for (int i = length - 1; i > 1; i--)
     {
-        int coeffLength = getNumberLength(coefficients[length - i - 1]);
-//        int degreeLenght = getNumberLength(i);
+    	int coeff = coefficients[length - i - 1];
+
+    	if (coeff == 0)
+    		continue;
+
+        int coeffLength = getNumberLength(coeff);
         int indent = coeffLength;
 
         if (i != length - 1)
-            indent += 3;
+            indent = (coeff == 1 || coeff == -1) ? indent + 2 : indent + 3;
         else
-            if (coefficients[0] < 0)
+            if (coefficients[0] < 0 && coeff != -1)
                 indent++;
 
         printSpaces(indent);
@@ -46,19 +64,38 @@ int main()
     }
     cout << endl;
 
-    for (int i = length - 1; i >= 0; i--)
+    if (coefficients[0] != 0)
+	    if (coefficients[0] == 1)
+	    	cout << "x";
+	    else if (coefficients[0] == -1)
+	    	cout << "-x";
+	    else
+	    	cout << coefficients[0] << "x";
+
+	int lastDegree = length - 2;
+    for (int i = length - 2; i >= 0; i--)
     {
-        if (i == length - 1)
-            cout << coefficients[0] << "x";
+    	if (coefficients[length - i -1] == 0)
+    		continue;
+
+    	int degreeLenght = getNumberLength(lastDegree);
+    	printSpaces(degreeLenght - 1);
+    	lastDegree = i;
+
+        if (coefficients[length - i - 1] < 0)
+        {
+            cout << " - ";
+        	if (coefficients[length - i - 1] != -1 || i == 0)
+        		cout << -coefficients[length - i - 1];
+        }
         else
         {
-            if (coefficients[length - i - 1] < 0)
-                cout << " - " << -coefficients[length - i - 1];
-            else
-                cout << " + " << coefficients[length - i - 1];
-            if (i != 0)
-                cout << "x";
+            cout << " + ";
+        	if (coefficients[length - i - 1] != 1 || i == 0)
+        		cout << coefficients[length - i - 1];
         }
+        if (i != 0)
+            cout << "x";
     }
     cout << endl << endl;
 
