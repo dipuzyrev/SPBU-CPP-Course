40d541c0863d726a98517e1009641092745a487d
Dmitry <dmitry@puzyrev.me>
2016-12-17 18:42:47 +0300

    10_02 | first release

---
+++ course_1/semester_1/hw10/10_02/Tree.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CodesList.h"
+
+struct Tree;
+
+Tree *createTree(char value);
+void deleteTree(Tree *&tree);
+
+void mergeTrees(Tree *baseTree, Tree *&treeToMerge);
+void fillCodesList(Tree *tree, CodesList *codes);
+void printTree(const char *fileName, Tree *t);
+Tree *loadTreeFromFile(const char *fileName);
