6b3082368a97ca1afd8714e55b9bdc2cad269473
Dmitry <puzyrev.dmitry@gmail.com>
2017-03-14 18:29:42 +0300

    hw1_task1

---
+++ course_1/semester_2/hw1/task_1/src/puzyrev/sem2/hw1/task1/Stack.java
@@ -0,0 +1,47 @@
+package puzyrev.sem2.hw1.task1;
+
+public class Stack<Type> {
+
+    private class StackElement {
+
+        private Type value;
+        private StackElement next;
+
+        public StackElement(Type value, StackElement next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public Type getValue() {
+            return value;
+        }
+
+        public StackElement getNext() {
+            return next;
+        }
+    }
+
+    private StackElement head;
+
+    public Stack() {
+        head = null;
+    }
+
+    public void push(Type value) {
+        head = new StackElement(value, head);
+    }
+
+    public Type pop() {
+        if (head == null) {
+            return null;
+        } else {
+            Type value = head.getValue();
+            head = head.getNext();
+            return value;
+        }
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+}
