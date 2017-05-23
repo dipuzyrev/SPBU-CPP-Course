be8c53eea2cfeb3b03fc3b6f42a7bb4cfaf7994f
Dmitry <puzyrev.dmitry@gmail.com>
2017-05-23 23:28:03 +0300

    hw2_task1: save project structure

--- course_1/semester_2/hw2/task_1/src/puzyrev/sem2/hw2/task1/Main.java
+++ course_1/semester_2/hw2/task_1/src/puzyrev/sem2/hw2/task1/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter string to get answer");
-        System.out.println("Please, use only single-character numbers");
-        System.out.println("To stop enter 0");
+        System.out.println("Enter string to get answer.");
+        System.out.println("Please, use only single-character numbers.");
+        System.out.println("To stop enter 0.");
 
         String string = sc.nextLine();
         while (!string.equals("0")) {
--- course_1/semester_2/hw2/task_1/src/puzyrev/sem2/hw2/task1/Stack.java
+++ course_1/semester_2/hw2/task_1/src/puzyrev/sem2/hw2/task1/Stack.java
@@ -1,7 +1,5 @@
 package puzyrev.sem2.hw2.task1;
 
-import java.util.EmptyStackException;
-
 public interface Stack<Type> {
     void push(Type value);
     Type pop();
