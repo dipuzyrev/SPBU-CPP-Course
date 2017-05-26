547920bf38473bb90d6d8ad9ff857c5554a1e578
Dmitry <puzyrev.dmitry@gmail.com>
2017-05-26 10:29:27 +0300

    Added more tests.

--- course_1/semester_2/hw2/task_1/test/puzyrev/sem2/hw2/task1/CalculatorTest.java
+++ course_1/semester_2/hw2/task_1/test/puzyrev/sem2/hw2/task1/CalculatorTest.java
@@ -5,11 +5,43 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-    String testExpression = "3 * (4 + 6) / 5";
-    int correctAnswer = 6;
+    @Test
+    public void testPlus() {
+        String testExpression = "4 + 5";
+        int correctAnswer = 9;
+
+        assertTrue(correctAnswer == Calculator.getAnswer(testExpression));
+    }
 
     @Test
-    public void calculatorTest() {
+    public void testMinus() {
+        String testExpression = "9 - 2";
+        int correctAnswer = 7;
+
+        assertTrue(correctAnswer == Calculator.getAnswer(testExpression));
+    }
+
+    @Test
+    public void testMult() {
+        String testExpression = "4 * 9";
+        int correctAnswer = 36;
+
+        assertTrue(correctAnswer == Calculator.getAnswer(testExpression));
+    }
+
+    @Test
+    public void testDivision() {
+        String testExpression = "15 / 3";
+        int correctAnswer = 5;
+
+        assertTrue(correctAnswer == Calculator.getAnswer(testExpression));
+    }
+
+    @Test
+    public void testBigExpression() {
+        String testExpression = "3 * (4 + 6) / 5";
+        int correctAnswer = 6;
+
         assertTrue(correctAnswer == Calculator.getAnswer(testExpression));
     }
 }
