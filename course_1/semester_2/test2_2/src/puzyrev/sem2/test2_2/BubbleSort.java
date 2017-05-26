a93d6ae48ba83b14e1afcada8389c8654a583212
Dmitry <puzyrev.dmitry@gmail.com>
2017-05-26 11:30:19 +0300

    Add comparable class to test.

--- course_1/semester_2/test2_2/src/puzyrev/sem2/test2_2/BubbleSort.java
+++ course_1/semester_2/test2_2/src/puzyrev/sem2/test2_2/BubbleSort.java
@@ -3,9 +3,15 @@ package puzyrev.sem2.test2_2;
 import java.util.Comparator;
 
 /**
- * Bubble sorting for any types of arrays.
+ * Bubble sorting for arrays with elements of any types.
  */
 public class BubbleSort<T> {
+
+    /**
+     * Sort array.
+     * @param array array for sorting
+     * @param comparator comparator object for array elements
+     */
     public void sort(T[] array, Comparator<T> comparator) {
         for (int i = array.length; i > 0; i--) {
             for (int j = 0; j < i - 1; j++) {
--- course_1/semester_2/test2_2/test/puzyrev/sem2/test2_2/BubbleSortTest.java
+++ course_1/semester_2/test2_2/test/puzyrev/sem2/test2_2/BubbleSortTest.java
@@ -1,14 +1,14 @@
 package puzyrev.sem2.test2_2;
 
 import org.junit.jupiter.api.Test;
-
+import java.util.Comparator;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BubbleSortTest {
     @Test
     void testIntegers() {
-        Integer[] testAray = {5,3,-1,6,7};
-        Integer[] sortedArray = {-1,3,5,6,7};
+        Integer[] testAray = {5, 3, -1, 6, 7};
+        Integer[] sortedArray = {-1, 3, 5, 6, 7};
 
         BubbleSort<Integer> sorter = new BubbleSort<>();
         sorter.sort(testAray, Integer::compareTo);
@@ -41,6 +41,73 @@ class BubbleSortTest {
 
         for (int i = 0; i < testAray.length; i++) {
             assertTrue(testAray[i].equals(sortedArray[i]));
+        }
+    }
+
+    @Test
+    void testClass() {
+        TestClass[] testArray = {
+            new TestClass(2, 1),
+            new TestClass(3, 3),
+            new TestClass(1, 4),
+            new TestClass(1, 5),
+            new TestClass(1, 5),
+            new TestClass(3, 4)
+        };
+
+        BubbleSort<TestClass> sorter = new BubbleSort<>();
+        sorter.sort(testArray, new TestClass(0,0));
+
+        for (int i = 0; i < testArray.length - 1; i++) {
+            assertTrue(testArray[i].compareTo(testArray[i + 1]) <= 0);
+        }
+    }
+
+    private class TestClass implements Comparable<TestClass>, Comparator<TestClass> {
+        public int firstIndex;
+        public int secondIndex;
+
+        public TestClass(int first, int second) {
+            firstIndex = first;
+            secondIndex = second;
+        }
+
+        public void getText() {
+            System.out.println(firstIndex + "/" + secondIndex);
+        }
+
+        @Override
+        public int compareTo(TestClass element) {
+            if (firstIndex < element.firstIndex) {
+                return -1;
+            } else if (firstIndex > element.firstIndex) {
+                return 1;
+            } else {
+                if (secondIndex < element.secondIndex) {
+                    return -1;
+                } else if (secondIndex > element.secondIndex) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
+        @Override
+        public int compare(TestClass element1, TestClass element2) {
+            if (element1.firstIndex < element2.firstIndex) {
+                return -1;
+            } else if (element1.firstIndex > element2.firstIndex) {
+                return 1;
+            } else {
+                if (element1.secondIndex < element2.secondIndex) {
+                    return -1;
+                } else if (element1.secondIndex > element2.secondIndex) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
         }
     }
 }
